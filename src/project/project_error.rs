37e954d617d0085ba74e97d121393cd1c1199fe4
Milo Banks <plutoisaplanet4324@gmail.com>
2020-10-20 19:54:08 -0600

    laptop -> desktop

---
+++ src/project/project_error.rs
@@ -0,0 +1,19 @@
+use snafu::{ResultExt, Snafu};
+use std::{fs, io};
+
+#[derive(Debug, Snafu)]
+#[snafu(visibility(pub(crate)))]
+pub enum ProjectError {
+    #[snafu(display("Unable to read configuration from {}: {}", path, source))]
+    ReadFile {
+        source: Box<io::Error>,
+        path: String
+    },
+
+    #[snafu(display("Unable to parse configuration from {}: {}", path, source))]
+    ParseFile {
+        #[snafu(source(from(serde_yaml::Error, Box::new)))]
+        source: Box<serde_yaml::Error>,
+        path: String
+    },
+}
